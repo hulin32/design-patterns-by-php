f8665896dd5021fa55871d598c2e23b440e29cfc
flyingalex <hulinalex@163.com>
2016-12-02 21:34:52 +0800

    add chapter1

---
+++ Operation.php
@@ -0,0 +1,103 @@
+<?php 
+
+class Operation
+{
+    protected  $a = 0;
+    protected  $b = 0;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+    }
+
+    public function getResult()
+    {
+        $result = 0;
+        return $result;
+    }
+}
+
+/**
+*  add 
+*/
+class OperationAdd extends Operation
+{
+    public function getResult()
+    {
+        return $this->a + $this->b;
+    }
+}
+
+/**
+* Mul
+*/
+class OperationMul extends Operation
+{
+    public function getResult()
+    {
+        return $this->a * $this->b;
+    } 
+}
+
+/**
+* sub
+*/
+class OperationSub extends Operation
+{
+    public function getResult()
+    {
+        return $this->a - $this->b;
+    }
+}
+
+/**
+* div
+*/
+class OperationDiv extends Operation
+{
+    public function getResult()
+    {
+        $this->a / $this->b;
+    }
+}
+
+
+/**
+* operation factory
+*/
+class OperationFactory
+{
+    public static function createOperation($operation)
+    {
+        switch ($operation) {
+            case '+':
+                $oper = new OperationAdd();
+                break;
+            case '-':
+                $oper = new OperationSub();
+                break;
+            case '/':
+                $oper = new OperationDiv();
+                break;
+            case '*':
+                $oper = new OperationMul();
+                break;
+        }
+        return $oper;
+    }
+}
+
+$operation = OperationFactory::createOperation('+');
+$operation->setA(1);
+$operation->setA(2);
+echo $operation->getResult()."\n";
+
+
+
+
+
